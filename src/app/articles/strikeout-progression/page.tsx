0aae1b038ee1cf9b0a5e3d92856e0f5ef63b8e2e
Cole <cebCole@gmail.com>
2025-11-05 09:27:32 -0600

    proofread main page

--- src/app/articles/strikeout-progression/page.tsx
+++ src/app/articles/strikeout-progression/page.tsx
@@ -42,7 +42,7 @@ export default function ArticlePage() {
                         Strikeouts - Baseballs Chase for Ineptitude
                     </h1>
                     <p className="">By: ProbablyBaseball</p>
-                    <p className="mb-8">Published on October 20, 2025</p>
+                    <p className="mb-8">Published on November 11, 2025</p>
 
                     <section className="mb-12 text-wrap">
                         <h2 className="text-2xl font-semibold mb-3">
@@ -50,11 +50,11 @@ export default function ArticlePage() {
                         </h2>
                         <p className="text-lg leading-relaxed text-wrap">
                             The year is 1871. The player, Reinder &quot;Rynie&quot; Albertus Wolters, a 27 year-old Dutch standout
-                            pitching for the New York Mutuals. That year Rynie would go on to lead the league in games started,
-                            complete games, innings pitched, and tie for first in shutouts with 1. On the offensive side he would lead the league
+                            pitching for the New York Mutuals. That year, Rynie would go on to lead the league in games started,
+                            complete games, innings pitched, and tie for first in shutouts with 1. On the offensive side, he would lead the league
                             in RBI&apos;s with 44, while slashing a .370/.412/.543, good enough for an OPS/OPS+ of .956/182. He would also go on
-                            to strikeout 8 times, leading the league in that category on a team that would only strikeout 15 times in 33 games. This
-                            feat would set the stage for baseballs biggest, glorious and most prestigous title, The strikeout king.
+                            to strike out 8 times, leading the league in that category on a team that would only strike out 15 times in 33 games. This
+                            feat would set the stage for baseball&apos;s biggest, glorious and most prestigious title, The strikeout king.
                         </p>
                     </section>
                     <div id="toc">

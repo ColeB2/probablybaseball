b54e70866c38c4920b05b07497458c632a0ddad1
Cole <cebCole@gmail.com>
2026-03-11 09:05:20 -0500

    color changes link

--- src/components/Articles/Articles.tsx
+++ src/components/Articles/Articles.tsx
@@ -22,7 +22,7 @@ export default function ArticleList({ articles }: ArticleListProps) {
                 <div className="flex flex-col justify-center p-6 text-left">
                     <h2 className="text-2xl font-semibold mb-2">{article.title}</h2>
                     <p className="mb-4">{article.desc}</p>
-                    <span className="text-blue-600 dark:text-blue-300 font-medium hover:underline">
+                    <span className="text-blue-900 dark:text-blue-300 font-medium hover:underline">
                         Read more →
                     </span>
                 </div>
